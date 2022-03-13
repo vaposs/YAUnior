bf83f97eae8cb64aa96f605a2bc22d5d0da550d0
vaposs <63776520+vaposs@users.noreply.github.com>
2022-03-13 17:04:37 +0300

    Add files via upload

---
+++ Задание_4/Project_4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Project_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string zodiacSing;
+            int age;
+            string plaseOfWork;
+
+
+            Console.WriteLine("Как вас зовут?");
+            name = Console.ReadLine();
+            Console.WriteLine("Какой ваш знак зодиака?");
+            zodiacSing = Console.ReadLine();
+            Console.WriteLine("Сколько вам лет?");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Где вы работаете?");
+            plaseOfWork = Console.ReadLine();
+
+            Console.WriteLine($"Вас зовут "+ name + ", вам - "+ age + "год и вы " + zodiacSing + ". Вы работаете на " + plaseOfWork + ".");
+
+
+
+        }
+    }
+}
