e125d88a88a539708beee9d81822d4b6f10d6b20
vaposs <63776520+vaposs@users.noreply.github.com>
2022-03-13 17:10:58 +0300

    Add files via upload

---
+++ Задания_5/Project_5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Project_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Мордакин";
+            string surname = "Валентин";
+            string temporaryМariable;
+
+            Console.WriteLine(name + " " + surname);
+
+            temporaryМariable = name;
+            name = surname;
+            surname = temporaryМariable;
+
+            Console.WriteLine(name + " " + surname);
+        }
+    }
+}
