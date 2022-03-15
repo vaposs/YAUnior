60e4ac6f0551b238ace399cb47eb2e765bf96118
vaposs <63776520+vaposs@users.noreply.github.com>
2022-03-15 09:38:00 +0300

    Add files via upload

---
+++ Задание_7/Project_7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Project_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int gold;
+            int crystal;
+            int crystalPrise;
+
+
+            Console.WriteLine("Введите количество золота - ");
+            gold = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Укажите цену кристала - ");
+            crystalPrise = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("сколько кристалов вы хотите купить?");
+            crystal = Convert.ToInt32(Console.ReadLine());
+
+            if ((gold < crystalPrise) || (gold < (crystal*crystalPrise)))
+            {
+                Console.WriteLine("Не достаточно золота для покупки кристалов");
+            }
+            else
+            {
+                gold = gold - (crystal * crystalPrise);
+                Console.WriteLine("У вас: " + crystal + " кристалов, и " + gold + " золота.");
+            }
+        }
+    }
+}
