b6b69ca98e836757a97bbb06b0b5e497118a8fe1
vaposs <63776520+vaposs@users.noreply.github.com>
2022-03-15 10:08:30 +0300

    Add files via upload

---
+++ Задание_8/Project_8/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Project_8
+{
+    class Program
+    {
+        static void Main()
+        {
+            long people;
+            long hour;
+            long minutу;
+
+            Console.WriteLine("Сколько человек перед вами в очереди: ");
+            people = Convert.ToInt32(Console.ReadLine());
+            hour = (people * 10) / 60;
+            minutу = (people * 10) % 60;
+
+            if(hour == 0)
+            {
+                Console.WriteLine("Вы должны стоять в очереди " + minutу + " минут");
+            }
+            else if(hour == 1)
+            {
+                Console.WriteLine("Вы должны стоять в очереди " + hour + " час, и " + minutу + " минут");
+            }
+            else if (hour > 1)
+            {
+                Console.WriteLine("Вы должны стоять в очереди " + hour + " часа, и " + minutу + " минут");
+            }
+        }
+    }
+}
