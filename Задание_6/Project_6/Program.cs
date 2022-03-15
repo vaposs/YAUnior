4b51d09606d6767b11d093d48585c1c9ef884692
vaposs <63776520+vaposs@users.noreply.github.com>
2022-03-15 09:01:45 +0300

    Add files via upload

---
+++ Задание_6/Project_6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Project_6
+{
+    class Program
+    {
+        static void Main()
+        {
+            int picture = 52;
+            int line;
+            int extraPictures;
+
+            line = picture / 3;
+            extraPictures = picture % 3;
+
+            Console.WriteLine("заполненые рядов - "+ line );
+            Console.WriteLine("Лишние карты - " + extraPictures);
+        }
+    }
+}
