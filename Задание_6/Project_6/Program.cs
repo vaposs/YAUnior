1c0c465271da4357d405d823b8c7a9447afb4888
vaposs <63776520+vaposs@users.noreply.github.com>
2022-03-15 19:58:36 +0300

    Update Program.cs

--- Задание_6/Project_6/Program.cs
+++ Задание_6/Project_6/Program.cs
@@ -7,14 +7,14 @@ namespace Project_6
         static void Main()
         {
             int picture = 52;
-            int linesPictute;
+            int fullPictureLine;
             int extraPictures;
             int pictureInLine = 3;
 
-            linesPictute = picture / pictureInLine;
-            extraPictures = picture % pictureInLine;
+            linesPictute = picture / fullPictureLine;
+            extraPictures = picture % fullPictureLine;
 
-            Console.WriteLine("заполненые рядов - " + linesPictute);
+            Console.WriteLine("заполненые рядов - " + fullPictureLine);
             Console.WriteLine("Лишние карты - " + extraPictures);
         }
     }
