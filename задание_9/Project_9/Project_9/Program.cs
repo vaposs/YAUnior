d07e6b27f3b6c13bc18976acdf9aed67ee54ff62
vaposs <vaposs@bk.ru>
2022-09-02 09:36:08 +0300

    задание 9
    выполненое задание №9

---
+++ задание_9/Project_9/Project_9/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Project_9
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Сколько раз желаете запустить цыкл? - ");
+            int cycle = Convert.ToInt32(Console.ReadLine());
+            for(int i = 0; cycle > i; cycle--)
+            {
+                Console.WriteLine($"выведеая строка №{cycle}");
+            }
+        }
+    }
+}
