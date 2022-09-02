5bb228afbb0365969b2e1e6f3c59259283576bf5
vaposs <vaposs@bk.ru>
2022-09-02 11:48:34 +0300

    задание 10

---
+++ Задание_10/Задание_10/Project_10/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Project_10
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            bool cycleActive = true;
+
+            while(cycleActive)
+            {
+                Console.Write("Введите любое значение для повтора цикла или 'exit' для выхода. - ");
+                string textMessege = Console.ReadLine();
+
+                if(textMessege == "exit")
+                {
+                    cycleActive = false;
+                    Console.WriteLine("Вы вышли из цикла.");
+                }
+            }
+            
+        }
+    }
+}
