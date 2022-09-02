72f8655f9854b0cc12d985250f7b5fe4b9afb729
vaposs <vaposs@bk.ru>
2022-09-02 12:23:12 +0300

    +5

--- Задание_10/Задание_10/Project_10/Program.cs
+++ Задание_10/Задание_10/Project_10/Program.cs
@@ -11,7 +11,7 @@ namespace Project_10
 
             while(isActive)
             {
-                Console.Write("Введите любое значение для повтора цикла или 'exit' для выхода. - ");
+                Console.Write($"Введите любое значение для повтора цикла или '{stopCicle}' для выхода. - ");
                 string textMessege = Console.ReadLine();
 
                 if(textMessege == stopCicle)
