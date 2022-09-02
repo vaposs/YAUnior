4e967596eb3d10ae0bfc41ab27707885943f99ee
vaposs <vaposs@bk.ru>
2022-09-02 12:57:04 +0300

    +1

---
+++ задание_11/Project_11/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Project_11
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            bool isActive = true;
+            int numberOfIterations = 0;
+            int conditionDigit = 7;
+            int divisionRemainder = 5;
+            int maxDigit = 100;
+
+            Console.WriteLine($"Будем выводить числа с остатком от деления '{divisionRemainder}' и меньше чем '{maxDigit}'.");
+            Console.WriteLine("Нажмите Enter для старта.");
+            Console.WriteLine("Продолжить . . .");
+            Console.ReadLine();
+
+            while(isActive)
+            {
+                numberOfIterations++;
+                if ((numberOfIterations%conditionDigit == divisionRemainder)&&(numberOfIterations < maxDigit))
+                {
+                    Console.WriteLine($" Число - {numberOfIterations} ");
+                }
+                if(numberOfIterations > 100)
+                {
+                    Console.WriteLine("Вывод чисел закончен.");
+                    isActive = false;
+                }
+            }
+
+
+        }
+    }
+}
