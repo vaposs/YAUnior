4dbadc47490184661abb262fffffc26a2d4e4cc5
vaposs <vaposs@bk.ru>
2022-09-07 15:05:19 +0300

    +1

--- задание_13/Project_13/Program.cs
+++ задание_13/Project_13/Program.cs
@@ -11,17 +11,17 @@ namespace Project_13
             Random random = new Random();
             int maxNumber = random.Next(minRandom,maxRandom);
             int summNumbers = 0;
-            int multepleThree = 3;
-            int multepleFive = 5;
+            int multepleFirst = 3;
+            int multepleSecond = 5;
 
             for (int i = 1; i <= maxNumber; i++)
             {
-                if((i%multepleThree == 0)||(i%multepleFive == 0))
+                if((i%multepleFirst == 0)||(i%multepleSecond == 0))
                 {
                     summNumbers += i;
                 }
             }
-            Console.WriteLine($"Сумма всех натуральных числел от 1 до {maxNumber} которые кратны 3 или 5 равно - {summNumbers}");
+            Console.WriteLine($"Сумма всех натуральных числел от 1 до {maxNumber} которые кратны {multepleFirst} или {multepleSecond} равно - {summNumbers}");
         }
     }
 }
