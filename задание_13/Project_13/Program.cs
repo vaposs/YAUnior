6666d4b8b96382f4826a47baded4c95ea9904c12
vaposs <vaposs@bk.ru>
2022-09-07 14:45:33 +0300

    13

--- задание_13/Project_13/Program.cs
+++ задание_13/Project_13/Program.cs
@@ -10,14 +10,16 @@ namespace Project_13
             int maxRandom = 100;
             Random random = new Random();
             int maxNumber = random.Next(minRandom,maxRandom);
-            Console.WriteLine(maxNumber);
+            int summNumbers = 0;
+
             for(int i = 1; i <= maxNumber; i++)
             {
                 if((i%3 == 0)||(i%5 == 0))
                 {
-                    Console.WriteLine(i);
+                    summNumbers += i;
                 }
             }
+            Console.WriteLine($"Сумма всех натуральных числел от 1 до {maxNumber} которые кратны 3 или 5 равно - {summNumbers}");
         }
     }
 }
--- задание_20/Project_20/Program.cs
+++ задание_20/Project_20/Program.cs
@@ -6,7 +6,18 @@ namespace Project_20
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while(true)
+            {
+                Console.Write("введите строку для проверки - ");
+                string stringValidation = Console.ReadLine();
+
+                Array str = new Array[stringValidation.Length];
+
+                for (int i = 0; i < stringValidation.Length; i ++)
+                {
+                    str[i] = stringValidation[0];
+                }                
+            }
         }
     }
 }
