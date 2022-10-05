42c89e69853c191214703bce5a7f6e7b1e63cb2e
vaposs <vaposs@bk.ru>
2022-10-05 22:37:50 +0300

    2+2

--- 2 модуль/задание_2/Project_2/Program.cs
+++ 2 модуль/задание_2/Project_2/Program.cs
@@ -6,24 +6,25 @@ namespace Project_2
     {
         public static void Main(string[] args)
         {
-            int sizeArray = 10;
-            int maxNumber = 0;
+            int sizeArray = 5;
+            int maxNumber;
             int emptyNumber = 0;
+            int numberMax = 100;
 
             Random number = new Random();
             int[,] array = new int[sizeArray, sizeArray];
+            maxNumber = array[emptyNumber,emptyNumber];
 
             for (int i = 0; i < array.GetLength(0); i++)
             {
                 for (int j = 0; j < array.GetLength(1); j ++)
                 {
-                    array[i, j] = number.Next(100);
+                    array[i, j] = number.Next(numberMax);
                     Console.Write($"{array[i, j]} ");
 
                     if(array[i,j] > maxNumber)
                     {
                         maxNumber = array[i, j];
-                        array[i, j] = emptyNumber;
                     }
                 }
 
@@ -32,11 +33,14 @@ namespace Project_2
 
             Console.WriteLine("\n");
 
-
             for (int i = 0; i < array.GetLength(0); i++)
             {
                 for (int j = 0; j < array.GetLength(1); j++)
                 {
+                    if (array[i, j] == maxNumber)
+                    {
+                        array[i, j] = emptyNumber;
+                    }
                     Console.Write($"{array[i, j]} ");
                 }
 
