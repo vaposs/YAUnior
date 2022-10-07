5179c7d077e48444588613457e1ac940e6c1e1f2
vaposs <vaposs@bk.ru>
2022-10-07 23:01:46 +0300

    2+3

--- 2 модуль/задание_3/Project_3/Program.cs
+++ 2 модуль/задание_3/Project_3/Program.cs
@@ -7,32 +7,35 @@ namespace Project_3
         public static void Main(string[] args)
         {
             int arraySize = 30;
+            int arrayBegin = 0;
             int nextOrPrevious = 1;
+            int numberMax = 100;
             int[] array = new int[arraySize];
-            Random rand = new Random();
+            Random random = new Random();
 
-            for(int i = 0; i < array.Length; i ++)
+            int arrayLast = arraySize - nextOrPrevious;
+
+            for (int i = 0; i < array.Length; i++)
             {
-                array[i] = rand.Next(100);
+                array[i] = random.Next(numberMax);
                 Console.Write(array[i] + ",");
             }
 
-            Console.WriteLine("Hello World!");
+            Console.Write("\n\n");
 
-            for (int i = 0; i < array.Length; i++ )
+            if (array[arrayBegin + nextOrPrevious] < array[arrayBegin])
             {
-                if (i == 0)
-                {
-                    if (array[nextOrPrevious] < array[i])
-                    {
-                        Console.Write(array[i] + ",");
-                    }
-                }
-                else if (i == (arraySize - nextOrPrevious))
-                {
-                    Console.Write(array[i] + ",");
-                }
-                else if(array[i - nextOrPrevious] < array[i] && array[i + nextOrPrevious] < array[i])
+                Console.Write(array[arrayBegin] + ",");
+            }
+
+            if (array[arrayLast] > array[arrayLast - nextOrPrevious])
+            {
+                Console.Write(array[arrayLast] + ",");
+            }
+       
+            for (int i = 1; i < array.Length - 1; i++ )
+            {
+                if(array[i] > array[i + nextOrPrevious] && array[i] > array[i - nextOrPrevious])
                 {
                     Console.Write(array[i] + ",");
                 }
