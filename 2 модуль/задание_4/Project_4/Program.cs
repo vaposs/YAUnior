9285563b80c2cfc7de979a7eba35e6d79afd3ee7
vaposs <vaposs@bk.ru>
2022-10-09 11:50:08 +0300

    2+4

---
+++ 2 модуль/задание_4/Project_4/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Project_4
+{
+    class MainClass
+    {
+        public static void Main()
+        {
+            string enteredData;
+            int arraySize = 1;
+            int arraySizeNext = 1;
+            int sumInt = 0;
+            string exit = "exit";
+            string sumString = "sum";
+            int[] beginArray = new int[arraySize];
+            bool isNext = true;
+
+
+            while (isNext)
+            {
+                Console.Write("введите число или команду(sum/exit) - ");
+                enteredData = Console.ReadLine();
+
+                if (enteredData == exit)
+                {
+                    isNext = false;
+                }
+                else if (enteredData == sumString)
+                {
+                    for (int i = 0; i < beginArray.Length; i++)
+                    {
+                        sumInt += beginArray[i];
+                    }
+
+                    Console.WriteLine($"сумма чисел равна - {sumInt}");
+                }
+                else
+                {
+                    beginArray[arraySize - 1] = Convert.ToInt32(enteredData);
+                    int[] nextArray = new int[beginArray.Length + arraySizeNext];
+
+                    for(int i = 0; i < beginArray.Length; i ++)
+                    {
+                        nextArray[i] = beginArray[i];
+                    }
+
+                    arraySize++;
+                    beginArray = nextArray;
+
+                    for(int i = 0; i < beginArray.Length; i ++)
+                    {
+                        Console.Write(beginArray[i] + ",");
+                    }
+                }
+            }
+        }
+    }
+}
