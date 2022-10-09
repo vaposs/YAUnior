900ed2af082e394c90967aa4d5e020b06f639018
vaposs <vaposs@bk.ru>
2022-10-09 13:49:11 +0300

    2+5

---
+++ 2 модуль/задание_6/Project_6/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Project_6
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int startingPoint = 0;
+            int previousNumber = 1;
+            int temporaryVariable = 0;
+            int arraySize = 10;
+            int randomMax = 100;
+            Random random = new Random();
+            int[] array = new int[arraySize];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(randomMax);
+            }
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] < array[i - previousNumber])
+                {
+                    temporaryVariable = array[i];
+                    array[i] = array[i - previousNumber];
+                    array[i - previousNumber] = temporaryVariable;
+                    i = startingPoint;
+                }
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + ",");
+            }
+        }
+    }
+}
