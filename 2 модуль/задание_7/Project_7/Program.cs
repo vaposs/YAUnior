02657bc407c51c1450e2dc22beac9b2d63b145ea
vaposs <vaposs@bk.ru>
2022-10-15 01:13:03 +0300

    2+7

---
+++ 2 модуль/задание_7/Project_7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Project_7
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string line = "Таким образом, сложившаяся структура организации обеспечивает широкому кругу специалистов участие в формировании системы обучения кадров, соответствующей насущным потребностям? ";
+
+            string[] miniLine = line.Split(' ');
+
+            foreach (string word in miniLine)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
