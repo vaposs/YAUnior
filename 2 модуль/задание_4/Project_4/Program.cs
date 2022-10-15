4435dc8221ba11af5b8f478886c179ccebdb63d3
vaposs <vaposs@bk.ru>
2022-10-15 21:03:12 +0300

    2+4

--- 2 модуль/задание_4/Project_4/Program.cs
+++ 2 модуль/задание_4/Project_4/Program.cs
@@ -7,49 +7,43 @@ namespace Project_4
         public static void Main()
         {
             string enteredData;
-            int arraySize = 1;
-            string exit = "exit";
-            string sumString = "sum";
-            int sumInt = 0;
-            int[] beginArray = new int[arraySize];
-            bool isNext = true;
+            string commandExit = "exit";
+            string commandSum = "sum";
+            int sum = 0;
+            int[] array = new int[0];
+            bool isWork = true;
 
-            while (isNext)
+            while (isWork)
             {
-                Console.Write($"введите число или команду({sumString}/{exit}) - ");
+                Console.Write($"введите число или команду({commandSum}/{commandExit}) - ");
                 enteredData = Console.ReadLine();
-                
-                if (enteredData == exit)
+
+                if(enteredData == commandSum)
                 {
-                    isNext = false;
+                    sum = 0;
+
+                    for(int i = 0; i < array.Length; i++)
+                    {
+                        sum += array[i];
+                    }
+                    Console.WriteLine($"сумма чисел масива равна {sum}");
                 }
-                else if (enteredData == sumString)
+                else if(enteredData == commandExit)
                 {
-                    for (int i = 0; i < beginArray.Length; i++)
-                    {
-                        sumInt += beginArray[i];
-                    }
-
-                    Console.WriteLine($"сумма чисел равна - {sumInt}");
+                    isWork = false;
                 }
-                else
+                else if (Convert.ToInt32(enteredData) > int.MinValue && Convert.ToInt32(enteredData) < int.MaxValue)
                 {
-                    beginArray[arraySize - 1] = Convert.ToInt32(enteredData);
-                    int[] nextArray = new int[beginArray.Length + 1];
-
-                    for(int i = 0; i < beginArray.Length; i ++)
+                    int[] newArray = new int[array.Length + 1];
                     {
-                        nextArray[i] = beginArray[i];
-                    }
+                        for (int i = 0; i < array.Length; i++)
+                        {
+                            newArray[i] = array[i];
+                        }
 
-                    arraySize++;
-                    beginArray = nextArray;
-
-                    /*foreach (int number in beginArray)
-                    {
-                        Console.Write(number + ",");
+                        array = newArray;
+                        array[array.Length - 1] = Convert.ToInt32(enteredData);
                     }
-                    */
                 }
             }
         }
