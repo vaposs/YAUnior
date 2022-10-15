cd863d47f1e427cf8df5b3e1af4cc31b8ead7f0d
vaposs <vaposs@bk.ru>
2022-10-15 21:30:00 +0300

    2+8

---
+++ 2 модуль/задание_8/Project_8/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Project_8
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int[] array = new int[10];
+            int beginNamber;
+            int shift = 1;
+
+            Console.Write("первоначальный масив ");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = i;
+                Console.Write(array[i] + ",");
+            }
+
+            Console.Write("\nна сколько елементов сдвинуть? - ");
+            shift = Convert.ToInt32(Console.ReadLine());
+
+            while (shift > 0)
+            {
+                beginNamber = array[0];
+
+                for (int i = 0; i < array.Length; i ++)
+                {
+                    if(i == array.Length - 1)
+                    {
+                        array[i] = beginNamber;
+                    }
+                    else
+                    {
+                        array[i] = array[i + 1];
+                    }
+                }
+
+                shift--;
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + ",");
+            }
+        }
+    }
+}
