fa16c3d341ea802106de6c2f4b3d3c26ea199045
vaposs <vaposs@bk.ru>
2023-02-26 08:24:19 +0300

    5-1

--- 5 модуль (словари)/Project_5/Program.cs
+++ 5 модуль (словари)/Project_5/Program.cs
@@ -1,27 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Project_5
 {
-
-    //создать 2 масива
-    // заполнить рандомными числами 1-5
-    //обьеденить в один массив
-    //выполнить пузирьковую сортировку
-
-
     class MainClass
     {
         public static void Main(string[] args)
         {
             int sizeArrayFirst;
             int sizeArraySecond;
+            List<int> solution = new List<int>();
 
             Console.Write("Введите размер первого масива - ");
             sizeArrayFirst = GetNumber();
             Console.Write("Введите размер второго масива - ");
-            sizeArrayFirst = GetNumber();
-            Array[] arrayFirst = new Array[sizeArrayFirst];
+            sizeArraySecond = GetNumber();
+            int[] arrayFirst = new int[sizeArrayFirst];
+            int[] arraySecond = new int[sizeArraySecond];
+            int[] mergeArray = new int[sizeArrayFirst + sizeArraySecond];
 
+            FillArray(ref arrayFirst);
+            FillArray(ref arraySecond);
+            MergeArrays(ref arrayFirst, ref arraySecond, ref mergeArray);
+            BubbleSort(ref mergeArray);
+            TransferCollection(ref mergeArray, solution);
+            Console.Write("Результат - ");
+            PrintSolution(solution);
         }
 
         static int GetNumber()
@@ -46,6 +50,72 @@ namespace Project_5
                 }
             }
             return number;
+        }
+
+        static void MergeArrays(ref int[] arrayFirst, ref int[] arraySecond, ref int[] MergeArray)
+        {
+            for (int i = 0; i < MergeArray.Length; i++)
+            {
+                if (i < arrayFirst.Length)
+                {
+                    MergeArray[i] = arrayFirst[i];
+                }
+                else
+                {
+                    MergeArray[i] = arraySecond[i - arrayFirst.Length];
+                }
+
+            }
+        }
+
+        static void FillArray(ref int[] arrays)
+        {
+            Random numb = new Random();
+            int minRan = 1;
+            int maxRan = 10;
+
+            for (int i = 0; i < arrays.Length; i++)
+            {
+                arrays[i] = numb.Next(minRan,maxRan);
+            }
+        }
+
+        static void PrintSolution(List<int> solution)
+        {
+            foreach (var numb in solution)
+            {
+                Console.Write($"{numb} ,");
+            }
+        }
+
+        static void BubbleSort(ref int[] array)
+        {
+            int tempСell;
+
+            for (int i = 0; i < array.Length- 1; i++)
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if (array[i] > array[i + 1])
+                    {
+                        tempСell = array[i];
+                        array[i] = array[i + 1];
+                        array[i + 1] = tempСell;
+                        i = 0;
+                    }
+                }
+            }
+        }
+
+        static void TransferCollection(ref int[] array, List<int> solution)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if(solution.Contains(array[i]) == false)
+                {
+                    solution.Add(array[i]);
+                }
+            }
         }
     }
 }
