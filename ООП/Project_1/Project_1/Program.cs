32c3added11e0f1867b0551776b0a008680d9fb1
vaposs <vaposs@bk.ru>
2023-03-01 21:35:28 +0300

    1-4

--- ООП/Project_1/Project_1/Program.cs
+++ ООП/Project_1/Project_1/Program.cs
@@ -14,31 +14,26 @@ namespace Project_1
 
     class Player
     {
-        private int Health;
-        private int Armor;
-        private int Damage;
-        private int Speed;
-        private int Hanger;
-        private int Thirst;
+        private int _health;
+        private int _armor;
+        private int _damage;
+        private int _speed;
+        private int _hanger;
+        private int _thirst;
 
         public Player(int health, int armor, int damage, int speed, int hanger, int thirst)
         {
-            Health = health;
-            Armor = armor;
-            Damage = damage;
-            Speed = speed;
-            Hanger = hanger;
-            Thirst = thirst;
-        }
-
-        public Player()
-        {
-
+            _health = health;
+            _armor = armor;
+            _damage = damage;
+            _speed = speed;
+            _hanger = hanger;
+            _thirst = thirst;
         }
 
         public void ShowStats()
         {
-            Console.WriteLine($"Жизни - {Health}.\nЗащита - {Armor}.\nУрон - {Damage}.\nСкорость - {Speed}.\nГолод - {Hanger}.\nЖажда - {Thirst}.\n");
+            Console.WriteLine($"Жизни - {_health}.\nЗащита - {_armor}.\nУрон - {_damage}.\nСкорость - {_speed}.\nГолод - {_hanger}.\nЖажда - {_thirst}.\n");
         }
     }
 }
