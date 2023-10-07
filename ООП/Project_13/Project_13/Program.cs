1f80db674cc89cfe44f668ede0f3f075b6ffb56f
Vapos <Vaposs@bk.ru>
2023-10-07 13:26:02 +0300

    13-13

--- ООП/Project_13/Project_13/Program.cs
+++ ООП/Project_13/Project_13/Program.cs
@@ -1,24 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 
-// У вас есть автосервис, в который приезжают люди, чтобы починить свои автомобили.
-// У вашего автосервиса есть баланс денег и склад деталей.
-// Когда приезжает автомобиль, у него сразу ясна его поломка, и эта поломка отображается у вас в консоли вместе с ценой за починку(цена за починку складывается из цены детали + цена
-// за работу).
-//
-// Поломка всегда чинится заменой детали, но количество деталей ограничено тем, что находится на вашем складе деталей.
-// Если у вас нет нужной детали на складе, то вы можете отказать клиенту, и в этом случае вам придется выплатить штраф.
-// Если вы замените не ту деталь, то вам придется возместить ущерб клиенту.
-// За каждую удачную починку вы получаете выплату за ремонт, которая указана в чек-листе починки.
-// Класс Деталь не может содержать значение “количество”. Деталь всего одна, за количество отвечает тот, кто хранит детали. При необходимости можно создать дополнительный
-// класс для конкретной детали и работе с количеством.
-
-
-
-// классы - автосервис, заказчик, статические команды
-// у автосервыса ограниченый набор деталей
-// 
-
 namespace Project_13
 {
     class MainClass
@@ -32,64 +14,68 @@ namespace Project_13
 
         class UserUtils
         {
+            private static Dictionary<string, string> s_dicrionaryBreakdowns = new Dictionary<string, string>()
+            {
+                    { "РЕМОНТ ДВИГАТЕЛЕЙ", "запчасть двигателя"},
+                    { "РЕМОНТ АКПП", "запчасть АКПП"},
+                    { "РЕМОНТ СТАРТЕРОВ", "запчасть стартера"},
+                    { "РЕМОНТ ГЕНЕРАТОРОВ", "запчасть генератора"},
+                    { "РЕМОНТ ЭЛЕКТРОННЫХ БЛОКОВ", "запчасть електронных блоков"},
+                    { "ЧИСТКА ИНЖЕКТОРОВ", "запчасть для чистки инжектора"},
+                    { "ШИНОМОНТАЖ", "колесо"},
+                    { "КУЗОВНОЙ РЕМОНТ ЛЮБОЙ СЛОЖНОСТИ", "запчасть кузова"},
+                    { "ЗАМЕНА СТЕКОЛ", "стекло"},
+                    { "ХИМЧИСТКА","набор для химчистки"},
+                    { "ПОЛИРОВКА","набор для полировки"}
+            };
             private static Random s_random = new Random();
+            private static List<string> s_listBreakdowns = new List<string>();
+            private static List<string> s_listParts = new List<string>();
 
             public static int GenerateRandomNumber(int minRandomNumber, int maxRandomNumber)
             {
                 return s_random.Next(minRandomNumber, maxRandomNumber);
             }
 
-            public static int GetPositiveNumber()
+            public static void CreateListBreakdowns()
             {
-                string userInputString;
-                bool isConversionSucceeded = true;
-                bool isCorrectNumber;
-                int number = 0;
-
-                while (isConversionSucceeded)
+                foreach (var breakdown in s_dicrionaryBreakdowns)
                 {
-                    userInputString = Console.ReadLine();
-                    isCorrectNumber = int.TryParse(userInputString, out number);
-
-                    if (isCorrectNumber)
-                    {
-                        if (number < 1)
-                        {
-                            Console.Write("Неверный ввод. Число меньше единици. Повторите ввод - ");
-                        }
-                        else
-                        {
-                            isConversionSucceeded = false;
-                        }
-                    }
-                    else
-                    {
-                        Console.Write("Неверный ввод. Повторите ввод - ");
-                    }
+                    s_listBreakdowns.Add(breakdown.Key);
                 }
-
-                return number;
             }
 
-            public static Array ListBreakdowns()
+            public static List<string> GetListBreakdowns()
             {
-                string[] listBreakdowns = new string[]
-                {
-                    "РЕМОНТ ДВИГАТЕЛЕЙ",
-                    "РЕМОНТ АКПП",
-                    "РЕМОНТ СТАРТЕРОВ",
-                    "РЕМОНТ ГЕНЕРАТОРОВ",
-                    "РЕМОНТ ЭЛЕКТРОННЫХ БЛОКОВ",
-                    "ЧИСТКА ИНЖЕКТОРОВ",
-                    "ШИНОМОНТАЖ",
-                    "КУЗОВНОЙ РЕМОНТ ЛЮБОЙ СЛОЖНОСТИ",
-                    "ЗАМЕНА СТЕКОЛ",
-                    "ХИМЧИСТКА",
-                    "ПОЛИРОВКА"
-                };
-
-                return listBreakdowns;
+                return s_listBreakdowns;
             }
+
+            public static void CreateListPart()
+            {
+                foreach (var part in s_dicrionaryBreakdowns)
+                {
+                    s_listParts.Add(part.Value);
+                }
+            }
+
+            public static List<string> GetListPart()
+            {
+                return s_listParts;
+            }
+
+            public static string GetBreakdownClient()
+            {
+                string breakdown = s_listBreakdowns[GenerateRandomNumber(0, s_listBreakdowns.Count)];
+
+                return breakdown;
+             }
+
+            public static string GetNamePart(string breakdown)
+            {
+                s_dicrionaryBreakdowns.TryGetValue(breakdown, out string part);
+                return part;
+            }
+
         }
 
         class CarService
@@ -98,26 +84,55 @@ namespace Project_13
             Stock stock = new Stock();
             Queue<Client> _clients = new Queue<Client>();
             Dictionary<string, int> _breakdowns = new Dictionary<string, int>();
+            Client currentСlient;
 
             public void Work()
             {
-                stock.Creation();
+                UserUtils.CreateListBreakdowns();
+                UserUtils.CreateListPart();
+                stock.CreationPart();
                 CreateQueueClient();
                 CreateRepairPrice();
-                ShowQueue();
-                ShowPriceRepair();
-                stock.ShowPart();
-
-                while (_clients.Count < 0)
+                
+                while (_clients.Count > 0) 
                 {
+                    currentСlient = _clients.Dequeue();
+                    Console.WriteLine($"Деньги сервиса - {_serviseMoney}");
+                    ShowQueue();
+                    ShowPriceRepair();
+                    stock.ShowPart();
+                    currentСlient.ShowBreakdown();
+                    if(stock.GetSparePart(currentСlient.Breakdown))
+                    {
+                        _serviseMoney += _breakdowns[currentСlient.Breakdown];
+                    }
+                    else
+                    {
+                        _serviseMoney -= _breakdowns[currentСlient.Breakdown];
 
+                        if(_serviseMoney < 0)
+                        {
+                            Console.WriteLine("вы плохой управляющий, сервис разорился");
+                            _clients.Clear();
+                        }
+                    }
 
                     Console.ReadKey();
+                    Console.Clear();
                 }
 
-                Console.WriteLine("На сегодня клиенты закончились, пора домой");
+                if(_serviseMoney < 0)
+                {
+                    Console.WriteLine("вы пошли бомжевать");
+                }
+                else
+                {
+                    Console.WriteLine("На сегодня клиенты закончились, пора домой");
+                }
+
                 Console.ReadKey();
             }
+            
             private void ShowPriceRepair()
             {
                 Console.WriteLine("\nСтоимость ремонта:");
@@ -126,57 +141,57 @@ namespace Project_13
                 {
                     Console.WriteLine(item.Key + "\t" + item.Value);
                 }
-            }
-
+            } 
+            
             private void CreateQueueClient()
             {
-                int minPeople = 5;
-                int maxPeople = 30;
+                int minPeople = 970;
+                int maxPeople = 1000;
 
                 int queuePeople = UserUtils.GenerateRandomNumber(minPeople, maxPeople);
 
-                for (int i = 0; i < queuePeople; i++)
+                for (int i = 1; i < queuePeople; i++)
                 {
-                    _clients.Enqueue(new Client());
+                    _clients.Enqueue(new Client(i));
                 }
-            }
-
+            } 
+            
             private void ShowQueue()
             {
                 Console.WriteLine($"В очереди на ремонт {_clients.Count} клиентов");
-            }
-
+            } 
+            
             private void CreateRepairPrice()
             {
                 int minRepairPrice = 10;
                 int maxRepairPrice = 500;
 
-                string[] listBreakdowns = new string[]
-                {
-                    "РЕМОНТ ДВИГАТЕЛЕЙ",
-                    "РЕМОНТ АКПП",
-                    "РЕМОНТ СТАРТЕРОВ",
-                    "РЕМОНТ ГЕНЕРАТОРОВ",
-                    "РЕМОНТ ЭЛЕКТРОННЫХ БЛОКОВ",
-                    "ЧИСТКА ИНЖЕКТОРОВ",
-                    "ШИНОМОНТАЖ",
-                    "КУЗОВНОЙ РЕМОНТ ЛЮБОЙ СЛОЖНОСТИ",
-                    "ЗАМЕНА СТЕКОЛ",
-                    "ХИМЧИСТКА",
-                    "ПОЛИРОВКА"
-                };
+                List<string> listBreakdowns = UserUtils.GetListBreakdowns();
 
-                for (int i = 0; i < listBreakdowns.Length; i++)
+                for (int i = 0; i < listBreakdowns.Count; i++)
                 {
                     _breakdowns.Add(listBreakdowns[i], UserUtils.GenerateRandomNumber(minRepairPrice, maxRepairPrice));
                 }
-            }
+            } 
+
         }
 
         class Client
         {
-            
-        }
+            public Client(int number)
+            {
+                Number = number;
+                Breakdown = UserUtils.GetBreakdownClient();
+            }
+
+            public int Number { get; private set; }
+            public string Breakdown { get; private set; }
+
+            public void ShowBreakdown()
+            {
+                Console.WriteLine($"\nКлиент №{Number} с поломкой {Breakdown}");
+            }
+        } 
 
         class Stock
         {
@@ -185,9 +200,27 @@ namespace Project_13
             private int _minPart = 3;
             private int _maxPart = 15;
 
-            public void GetSparePart() // достаем запчасти
+            public bool GetSparePart(string breakdown)
             {
+                bool successfulRepair = true;
+                string part = UserUtils.GetNamePart(breakdown);
 
+                if (_parts.ContainsKey(part))
+                {
+                    int currentCount = _parts[part];
+
+                    if (currentCount > 0)
+                    {
+                        _parts[part]--;
+                    }
+                    else
+                    {
+                        successfulRepair = false;
+                        Console.WriteLine($"{part} закончилась");
+                    }
+                }
+             
+                return successfulRepair;
             }
 
             public void ShowPart()
@@ -197,37 +230,18 @@ namespace Project_13
                 {
                     Console.WriteLine($"{part.Key} в количестве - {part.Value}");
                 }
-            }
+            } 
 
-            public void Creation()
+            public void CreationPart()
             {
-                string[] parts = new string[]
-                {
-                    "запчасть двигателя",
-                    "запчасть АКПП",
-                    "запчасть стартера",
-                    "запчасть генератора",
-                    "запчасть електронных блоков",
-                    "запчасть для чистки инжектора",
-                    "колесо",
-                    "запчасть кузова",
-                    "стекло",
-                    "набор для химчистки",
-                    "набор для полировки"
-                };
+                List<string> parts = UserUtils.GetListPart();
 
-                for (int i = 0; i < parts.Length; i++)
+                for (int i = 0; i < parts.Count; i++)
                 {
                     _parts.Add(parts[i], UserUtils.GenerateRandomNumber(_minPart, _maxPart));
                 }
             }
+
         }
     }
 }
-
-/*
-T Dequeue(): извлекает и возвращает первый элемент очереди
-
-void Enqueue(T item): добавляет элемент в конец очереди
-
-*/
