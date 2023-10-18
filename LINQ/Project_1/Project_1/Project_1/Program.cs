fbe1b42e6b31e6c1612048e9314958da47173b15
Vapos <Vaposs@bk.ru>
2023-10-18 12:51:09 +0300

    1-1

--- LINQ/Project_1/Project_1/Project_1/Program.cs
+++ LINQ/Project_1/Project_1/Project_1/Program.cs
@@ -18,67 +18,59 @@ namespace Project_1
 
     class Database
     {
+        private SuspectCreator SuspectCreator = new SuspectCreator();
         private List<Suspect> _suspects = new List<Suspect>();
-        private List<string> _nationals;
+        private string[] _nationals;
 
         public void Work()
         {
             CreateListSuspect();
-            _nationals = CreateListNationals();
+            _nationals = SuspectCreator.GetAllNational();
+
             Console.WriteLine($"Количество подозреаемых - {_suspects.Count}");
-            ShowSuspect();
+            ShowSuspect(_suspects);
+
+            Console.WriteLine("Результат поиска:");
+            ShowSuspect(FilterSuspects());
+        }
+
+        private List<Suspect> FilterSuspects()
+        {
             Console.WriteLine("введите параметры поиска:");
             int height = GetSearchParameterHeight();
             int weight = GetSearchParameterWeight();
-            string national = GetSearchParameterNational();
+            string national = GetSearchParameterNational(_nationals);
 
             var filterSuspects = from Suspect suspect in _suspects
-                                 where GetSuspect(suspect, height, weight, national)
+                                 where suspect.Height == height
+                                        && suspect.Weight == weight
+                                        && suspect.Nationality == national
+                                        && suspect.Detained == false
                                  select suspect;
 
-            int index = 1;
-            Console.WriteLine(filterSuspects.Count());
-
-            foreach (var suspect in filterSuspects)
-            {
-                string status = GetText(suspect.Detained);
-                Console.WriteLine($"{index}.{suspect.Name} - {status} - {suspect.Height}/{suspect.Weight} - {suspect.Nationality}");
-                index++;
-            }
-        }
-
-        private bool GetSuspect(Suspect suspect, int height, int weight, string national)
-        {
-            if(suspect.Height == height && suspect.Weight == weight && suspect.Nationality == national && suspect.Detained == false)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+            return filterSuspects.ToList();
         }
 
         private int GetSearchParameterHeight()
         {
-            Console.Write("Введите рост подозреваемого (150/220) - ");
+            Console.Write($"Введите рост подозреваемого ({GetMinHeihtg()}/{GetMaxHeihtg()}) - ");
             return UserUtils.GetPositiveNumber();
         }
 
         private int GetSearchParameterWeight()
         {
-            Console.Write("Введите вес подозреваемого (55/100) - ");
+            Console.Write($"Введите вес подозреваемого ({GetMinWeight()}/{GetMaxWeight()}) - ");
             return UserUtils.GetPositiveNumber();
         }
 
-        private string GetSearchParameterNational()
+        private string GetSearchParameterNational(string[] nationals)
         {
             string choceDetective = "";
-            bool goodChoce = true;
+            bool isGoodChoce = true;
 
-            while (goodChoce)
+            while (isGoodChoce)
             {
-                foreach (string national in _nationals)
+                foreach (string national in nationals)
                 {
                     Console.Write(national + ", ");
                 }
@@ -86,11 +78,11 @@ namespace Project_1
                 Console.Write("\nвведите национальность подозреваемого из представленого списка:");
                 choceDetective = Console.ReadLine();
 
-                foreach (string national in _nationals)
+                foreach (string national in nationals)
                 {
                     if (national == choceDetective)
                     {
-                        goodChoce = false;
+                        isGoodChoce = false;
                         break;
                     }
                 }
@@ -99,74 +91,56 @@ namespace Project_1
             return choceDetective;
         }
 
-        private List<string> CreateListNationals()
-        {
-            List<string> nationals = new List<string>();
-            bool repick;
-
-            foreach (Suspect suspect in _suspects)
-            {
-                repick = false;
-
-                if(nationals.Count < 1)
-                {
-                    nationals.Add(suspect.Nationality);
-                    repick = true;
-                }
-                else
-                {
-                    foreach (string national in nationals)
-                    {
-                        if(suspect.Nationality == national)
-                        {
-                            repick = true;
-                        }
-                    }
-                }
-
-                if (repick == false)
-                {
-                    nationals.Add(suspect.Nationality);
-                }
-            }
-
-            return nationals;
-        }
-
         private void CreateListSuspect()
         {
-            int minSuspects = 10000;
-            int maxSuspects = 15000;
+            int minSuspects = 100;
+            int maxSuspects = 150;
             int countSuspects = UserUtils.GenerateRandomNumber(minSuspects, maxSuspects);
 
             for (int i = 0; i < countSuspects; i++)
             {
-                _suspects.Add(new Suspect());
+                _suspects.Add(SuspectCreator.Creator());
             }
         }
 
-        private void ShowSuspect()
+        private void ShowSuspect(List<Suspect> suspects)
         {
             int index = 1;
 
-            foreach (Suspect suspect in _suspects)
+            foreach (Suspect suspect in suspects)
             {
-                string status = GetText(suspect.Detained);
+                string status = (suspect.Detained == true) ? "задержан" : "на свободе";
                 Console.WriteLine($"{index}.{suspect.Name} - {status} - {suspect.Height}/{suspect.Weight} - {suspect.Nationality}");
                 index++;
             }
         }
 
-        private string GetText(bool status)
+        private int GetMaxHeihtg()
         {
-            if(status == true)
-            {
-                return "задержан";
-            }
-            else
-            {
-                return "на свободе";
-            }
+            var maxHeight = _suspects.Max(suspect => suspect.Height);
+
+            return maxHeight;
+        }
+
+        private int GetMinHeihtg()
+        {
+            var maxHeight = _suspects.Min(suspect => suspect.Height);
+
+            return maxHeight;
+        }
+
+        private int GetMaxWeight()
+        {
+            var maxWeight = _suspects.Max(suspect => suspect.Weight);
+
+            return maxWeight;
+        }
+
+        private int GetMinWeight()
+        {
+            var maxWeight = _suspects.Min(suspect => suspect.Weight);
+
+            return maxWeight;
         }
     }
 
@@ -181,24 +155,22 @@ namespace Project_1
 
         public static bool GenerateRandomBool()
         {
-            bool[] logic = {true, false };
+            bool[] logic = { true, false };
 
-            return logic[s_random.Next(0,logic.Length)];
+            return logic[s_random.Next(0, logic.Length)];
         }
 
         public static int GetPositiveNumber()
         {
             string readName;
             bool isConversionSucceeded = true;
-            bool isCorrectNumber;
             int number = 0;
 
             while (isConversionSucceeded)
             {
                 readName = Console.ReadLine();
-                isCorrectNumber = int.TryParse(readName, out number);
 
-                if (isCorrectNumber)
+                if (int.TryParse(readName, out number))
                 {
                     if (number < 1)
                     {
@@ -221,18 +193,13 @@ namespace Project_1
 
     class Suspect
     {
-        private int minHeight = 150;
-        private int maxHeight = 220;
-        private int minWeight = 55;
-        private int maxWeight = 100;
-
-        public Suspect()
+        public Suspect(string name, int height, int weight, string national, bool detained)
         {
-            Name = GetName();
-            Detained = UserUtils.GenerateRandomBool();
-            Height = UserUtils.GenerateRandomNumber(minHeight, maxHeight);
-            Weight = UserUtils.GenerateRandomNumber(minWeight, maxWeight);
-            Nationality = GetNationality();
+            Name = name;
+            Detained = detained;
+            Height = height;
+            Weight = weight;
+            Nationality = national;
         }
 
         public string Name { get; private set; }
@@ -241,6 +208,23 @@ namespace Project_1
         public int Weight { get; private set; }
         public string Nationality { get; private set; }
 
+
+    }
+
+    class SuspectCreator
+    {
+        private string[] _nationals = new string[] { "HKG", "GRL", "GEO", "DNK", "UK" };
+
+        public Suspect Creator()
+        {
+            return new Suspect(GetName(), GetHeight(), GetWeight(), GetNationality(), UserUtils.GenerateRandomBool());
+        }
+
+        private string GetNationality()
+        {
+            return _nationals[UserUtils.GenerateRandomNumber(0, _nationals.Length)];
+        }
+
         private string GetName()
         {
             string[] names = new string[] { "Петя", "Филя", "Семен", "Вася", "Степа" };
@@ -248,11 +232,25 @@ namespace Project_1
             return names[UserUtils.GenerateRandomNumber(0, names.Length)];
         }
 
-        private string GetNationality()
+        private int GetHeight()
         {
-            string[] names = new string[] { "HKG", "GRL", "GEO", "DNK", "UK" };
+            int minHeight = 150;
+            int maxHeight = 220;
 
-            return names[UserUtils.GenerateRandomNumber(0, names.Length)];
+            return UserUtils.GenerateRandomNumber(minHeight, maxHeight);
+        }
+
+        private int GetWeight()
+        {
+            int minWeight = 55;
+            int maxWeight = 100;
+
+            return UserUtils.GenerateRandomNumber(minWeight, maxWeight);
+        }
+
+        public string[] GetAllNational()
+        {
+            return _nationals;
         }
     }
 }
