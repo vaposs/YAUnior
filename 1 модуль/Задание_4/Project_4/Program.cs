4e1eaf5ccbcb265af7e215063f5a83d47e978690
vaposs <63776520+vaposs@users.noreply.github.com>
2026-01-14 16:54:17 +0300

    Update Program.cs

--- 1 модуль/Задание_4/Project_4/Program.cs
+++ 1 модуль/Задание_4/Project_4/Program.cs
@@ -8,14 +8,14 @@ namespace asdasf
         {
             byte userAge = 32;
             int userMoney = 100;
-            long galaxyStarCount = 1234567891234567890;
+            long totalStarsInGalaxy = 1234567891234567890;
             uint currentYear = 2022;
             float divisionResult = 2.5f;
             double piValue = 3.1415;
             bool isBlocked = true;
-            bool canCharacterWalk = false;
+            bool isCharacterWalk = false;
             char firstAlphabetLetter = 'a';
-            string userMotivation = "I to be coder!";
+            string motivationText = "I to be coder!";
         }
     }
 }
