b0de4f399f9c27e8c32eddb785d32b75ce170898
vaposs <63776520+vaposs@users.noreply.github.com>
2026-01-17 15:46:53 +0300

    Update Program.cs

--- 1 модуль/Задание_9/Project_9/Project_9/Program.cs
+++ 1 модуль/Задание_9/Project_9/Project_9/Program.cs
@@ -6,12 +6,12 @@ namespace Project_9
     {
         public static void Main(string[] args)
         {
-            Console.Write("Сколько раз желаете запустить цыкл? - ");
+            Console.Write("Сколько раз желаете запустить цикл? - ");
             int numberOfIterations = Convert.ToInt32(Console.ReadLine());
 
-            for(int i = 0; numberOfIterations > i; numberOfIterations--)
+            for (int i = 1; i <= numberOfIterations; i++)
             {
-                Console.WriteLine($"выведеая строка №{numberOfIterations}");
+                Console.WriteLine($"строка №{i}");
             }
         }
     }
