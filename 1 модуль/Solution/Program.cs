8881ea25a138d0280fe1d7915644e311b63deb12
vaposs <63776520+vaposs@users.noreply.github.com>
2026-01-17 16:03:28 +0300

    add new solution

---
+++ 1 модуль/Solution/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ZD
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // мы используем for так как знаем начало и конец цикла
+
+            int startNumber = 5;
+            int endNumber = 103;
+            int step = 7;
+
+            for (int i = startNumber; i <= endNumber; i+= step)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
