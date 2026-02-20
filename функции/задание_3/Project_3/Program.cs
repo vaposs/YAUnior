a0832744b92fe5967d9135ac7d713d58ee3097ee
vaposs <63776520+vaposs@users.noreply.github.com>
2026-02-20 18:33:34 +0300

    Update Program.cs

--- функции/задание_3/Project_3/Program.cs
+++ функции/задание_3/Project_3/Program.cs
@@ -1,26 +1,31 @@
-﻿using System;
+using System;
 
-namespace Project_11
+class Program
 {
-    class MainClass
+    static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            int conditionDigit = 7;
-            int divisionRemainder = 5;
-            int maxDigit = 100;
-
-            Console.WriteLine($"Будем выводить числа с остатком от деления '{divisionRemainder}' и меньше чем '{maxDigit}'.");
-            Console.WriteLine("Нажмите Enter для старта.");
-            Console.WriteLine("Продолжить . . .");
-            Console.ReadLine();
-
-            for (int numberOfIterations = 5; maxDigit > numberOfIterations; numberOfIterations += conditionDigit)
-            {
-                Console.WriteLine($" Число - {numberOfIterations}");
-            }
-            Console.WriteLine("Вывод окончен.");
-            Console.ReadKey();
-        }
+        int userNumber = GetNumberFromUser();
+        Console.WriteLine($"Вы ввели число: {userNumber}");
     }
-} 
+
+    static int GetNumberFromUser()
+    {
+        bool isParsingSuccessful = false;
+        int number = 0;
+
+        while (isParsingSuccessful == false)
+        {
+            Console.Write("Введите целое число: ");
+            string userInput = Console.ReadLine();
+
+            isParsingSuccessful = int.TryParse(userInput, out number);
+
+            if (isParsingSuccessful == false)
+            {
+                Console.WriteLine("Ошибка! Введено некорректное число. Попробуйте снова.");
+            }
+        }
+
+        return number;
+    }
+}
