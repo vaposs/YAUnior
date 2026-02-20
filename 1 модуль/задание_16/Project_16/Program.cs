0a97a64918c49c13d00a478585a2fcd28693b875
vaposs <63776520+vaposs@users.noreply.github.com>
2026-02-20 16:11:50 +0300

    Update Program.cs

--- 1 модуль/задание_16/Project_16/Program.cs
+++ 1 модуль/задание_16/Project_16/Program.cs
@@ -1,11 +1,4 @@
-﻿using System;
-
-namespace задание_16
-{
-    class MainClass
-    {
-        public static void Main(string[] args)
-        {using System;
+using System;
 
 namespace задание_16
 {
@@ -26,29 +19,6 @@ namespace задание_16
             Console.WriteLine(middleLine);
             Console.WriteLine(borderLine);
 
-            Console.ReadKey();
-        }
-    }
-}
-            const int firstAndLastChar = 2;
-            Console.Write("Введите имя - ");
-            string name = Console.ReadLine();
-            Console.Write("Введите символ - ");
-            char frameSymbol = Convert.ToChar(Console.ReadLine());
-
-            int charToLine = name.Length + firstAndLastChar;
-
-            for (int i = 0; i < charToLine; i++)
-            {
-                Console.Write(frameSymbol);
-            }
-
-            Console.Write($"\n{frameSymbol}{name}{frameSymbol}\n");
-
-            for (int i = 0; i < charToLine; i++)
-            {
-                Console.Write(frameSymbol);
-            }
             Console.ReadKey();
         }
     }
