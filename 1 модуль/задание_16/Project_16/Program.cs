d575af0366b79d2300873bb3518f3dd7f098b049
vaposs <63776520+vaposs@users.noreply.github.com>
2026-02-20 16:11:14 +0300

    Update Program.cs

--- 1 модуль/задание_16/Project_16/Program.cs
+++ 1 модуль/задание_16/Project_16/Program.cs
@@ -5,7 +5,31 @@ namespace задание_16
     class MainClass
     {
         public static void Main(string[] args)
+        {using System;
+
+namespace задание_16
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
         {
+            Console.Write("Введите имя - ");
+            string name = Console.ReadLine();
+
+            Console.Write("Введите символ - ");
+            char frameSymbol = Console.ReadKey(true).KeyChar;
+
+            string middleLine = $"{frameSymbol}{name}{frameSymbol}";
+            string borderLine = new string(frameSymbol, middleLine.Length);
+
+            Console.WriteLine(borderLine);
+            Console.WriteLine(middleLine);
+            Console.WriteLine(borderLine);
+
+            Console.ReadKey();
+        }
+    }
+}
             const int firstAndLastChar = 2;
             Console.Write("Введите имя - ");
             string name = Console.ReadLine();
