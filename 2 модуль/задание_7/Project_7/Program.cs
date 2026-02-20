e519cdc398a0b40f4d1f978cb30b96deb512bc74
vaposs <63776520+vaposs@users.noreply.github.com>
2026-02-20 18:14:01 +0300

    Update Program.cs

--- 2 модуль/задание_7/Project_7/Program.cs
+++ 2 модуль/задание_7/Project_7/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Project_7
 {
@@ -7,8 +7,8 @@ namespace Project_7
         public static void Main(string[] args)
         {
             string line = "Таким образом, сложившаяся структура организации обеспечивает широкому кругу специалистов участие в формировании системы обучения кадров, соответствующей насущным потребностям? ";
-
-            string[] miniLine = line.Split(' ');
+            char separator = ' ';
+            string[] miniLine = line.Split(separator);
 
             foreach (string word in miniLine)
             {
